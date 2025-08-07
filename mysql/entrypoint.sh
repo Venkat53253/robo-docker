c3313bbd4490288cd80447629f779677a7c3a3d9
msivakumarreddy <info@joindevops.com>
2025-08-07 09:06:09 +0530

    terraform

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -7,8 +7,7 @@ else
     echo "Password file not found"
     exit 1
 fi
-
 # Making it as available in env
 export MYSQL_ROOT_PASSWORD=$PASSWORD
-
+rm -rf /tmp/mysql_root_password.txt
 exec /entrypoint.sh mysqld
